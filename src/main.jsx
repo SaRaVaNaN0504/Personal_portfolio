b522e701417aca78c8018749b45b307ce1405ca9
SARAVANAN <ksarv511@gmail.com>
2025-11-19 23:00:03 +0530

    update: modified src/main.jsx for analytics integration

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./assets/css/index.css";
 
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
